0b67d78d68348585eb44835b478e463bd7e2def5
Riju Nayak <riju.nayak@gmail.com>
2020-10-27 11:15:15 +0530

    Add test target for coverage

--- Demo.FunctionApp/Functions/GreetFunctions.cs
+++ Demo.FunctionApp/Functions/GreetFunctions.cs
@@ -27,7 +27,6 @@ namespace Demo.FunctionApp.Functions
             return name != null
                 ? (ActionResult) new OkObjectResult($"Hello, {name}!")
                 : new BadRequestObjectResult("Please pass a name on the query string or in the request body");
-            
         }
     }
 }
